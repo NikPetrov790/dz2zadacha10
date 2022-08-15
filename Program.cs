3779fb4ab7eb816b4210544199e15d332e783789
Petrov Nikolay <nikolay.petrov99@mail.ru>
2022-08-15 14:46:17 +0300

    second commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int numbers = Convert.ToInt32(Console.ReadLine());
+string threeNumbers = Convert.ToString(numbers);
+Console.Write("Вторая цифра -  "+ threeNumbers[1]);
+git commit -m "first commit"
